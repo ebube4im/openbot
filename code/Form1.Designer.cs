2437d49310f484799787a0aabc3810ea5d2ea475
KaanGaming <kaangaming.gamejolt@gmail.com>
2019-02-04 07:47:55 +0300

    Add files via upload
    Add the code files.

---
+++ code/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace OpenBot
+{
+	partial class Form1
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.txtEmail = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtWorldID = new System.Windows.Forms.TextBox();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(213, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Information";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// txtEmail
+			// 
+			this.txtEmail.Location = new System.Drawing.Point(13, 42);
+			this.txtEmail.Name = "txtEmail";
+			this.txtEmail.Size = new System.Drawing.Size(207, 20);
+			this.txtEmail.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(253, 45);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "E-mail";
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Location = new System.Drawing.Point(13, 68);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.Size = new System.Drawing.Size(207, 20);
+			this.txtPassword.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(235, 71);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Password";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(239, 97);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(49, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "World ID";
+			// 
+			// txtWorldID
+			// 
+			this.txtWorldID.Location = new System.Drawing.Point(13, 94);
+			this.txtWorldID.Name = "txtWorldID";
+			this.txtWorldID.Size = new System.Drawing.Size(207, 20);
+			this.txtWorldID.TabIndex = 5;
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.Location = new System.Drawing.Point(97, 140);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(123, 57);
+			this.btnConnect.TabIndex = 7;
+			this.btnConnect.Text = "Connect!";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			this.btnConnect.Click += new System.EventHandler(this.Connect);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(300, 450);
+			this.Controls.Add(this.btnConnect);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtWorldID);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtPassword);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txtEmail);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "OpenBot - An open-source bot";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox txtEmail;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtPassword;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtWorldID;
+		private System.Windows.Forms.Button btnConnect;
+	}
+}
+
---
+++ code/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using PlayerIOClient; // magic being implemented
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace OpenBot
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		Connection con;
+		Client client;
+		bool isConnected = false;
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("You can access the source by visiting https://github.com/skull111-co/openbot.");
+		}
+
+		private void Connect(object sender, EventArgs e) // going to be included as "Click" event for btnConnect, see in Form1's designer class.
+		{
+			if (!isConnected)
+			{
+				try
+				{
+					client = PlayerIO.QuickConnect.SimpleConnect("everybody-edits-su9rn58o40itdbnw69plyw", txtEmail.Text, txtPassword.Text, null);
+					con = client.Multiplayer.CreateJoinRoom(txtWorldID.Text, "public", true, new Dictionary<string, string>(), new Dictionary<string, string>());
+
+					con.Send("init");
+					con.OnMessage += new MessageReceivedEventHandler(handlemsg);
+					isConnected = true;
+					btnConnect.Text = "Disconnect";
+				}
+				catch
+				{
+					MessageBox.Show("Error!");
+				}
+			}
+			else
+			{
+				con.Send("say", "[OpenBot] Disconnected!");
+				con.Disconnect();
+				isConnected = false;
+				btnConnect.Text = "Connect!";
+			}
+		}
+
+		// the message handler will receive messages here!
+		// if any events possible from the protocol, then it will be quickly signed here.
+		//
+		// EXAMPLE:  if (m.Type == "say")
+		//           {
+		//               if (m.GetString(1) == "!ping") con.Send("say", "[OpenBot] Pong!");
+		//           }
+		//
+		private void handlemsg(object sender, PlayerIOClient.Message m)
+		{
+			if (m.Type == "say")
+			{
+				// add the rest commands with
+				//
+				// else if (m.GetString(1) == "!mycommand")
+				// {
+				//     con.Send("say", "[OpenBot] My command! OH NO!");
+				// }
+				//
+				// You can also make a seperate class file to shorten your "else if" condition AKA command script.
+				//
+				if (m.GetString(1) == "!ping")
+				{
+					con.Send("say", "[OpenBot] Pong!");
+					Thread.Sleep(100);
+					con.Send("say", "[OpenBot] ...did I just said that?");
+				}
+				else if (m.GetString(1) == "!github")
+				{
+					con.Send("say", "[OpenBot] Get this bot at https://github.com/skull111-co/openbot");
+				}
+				else if (m.GetString(1) == "!help")
+				{
+					con.Send("say", "[OpenBot] !github, !ping, !help");
+					// Make sure to include your command after implementing to the bot.
+					// If you don't want to, then this is counted as an "easter egg".
+				}
+			}
+			else if (m.Type == "init")
+			{
+				con.Send("say", "[OpenBot] Joined!");
+				con.Send("init2"); // joined successfully
+			}
+		}
+	}
+}
